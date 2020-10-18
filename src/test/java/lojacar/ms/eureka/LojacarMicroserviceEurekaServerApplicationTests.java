627846cc9921b1e0568af4bfab0d56039f920211
Cristian <crgaonas24@gmail.com>
2020-10-18 00:21:56 -0500

    Configurando servidor de descubrimiento

---
+++ src/test/java/lojacar/ms/eureka/LojacarMicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lojacar.ms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojacarMicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
